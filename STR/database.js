c05644720164fce6ab221825504ee60d25864c22
Nicholas Lemos <40637239+nicholemos@users.noreply.github.com>
2026-06-24 13:41:47 -0300

    Add files via upload

--- STR/database.js
+++ STR/database.js
@@ -1,5 +1,153 @@
 const DATABASE = [
   {
+    "id": "db228",
+    "label": "DB 228 (Jun/26)",
+    "artigos": [
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "magia",
+          "combate",
+          "regras"
+        ],
+        "pergunta": "Boa noite, conselheiros! Acabei de ter uma situação inusitada na minha mesa. Se eu lançar a magia Tentáculos de Trevas para enfrentar mortos-vivos, os tentáculos conseguem agarrar os mortos-vivos?",
+        "conselheiro": "— Cons. Matheus Cezar",
+        "resposta": "Saudações necromânticas, conselheiro! Mortos-vivos são imunes a todas as magias de necromancia, incluindo Tentáculos de Trevas."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "itens",
+          "combate",
+          "regras",
+          "distincao"
+        ],
+        "pergunta": "Após algumas jogatinas, eis que surgem algumas dúvidas deste grupo humilde. Então não resta alternativa a não ser recorrer ao Supremo.<br>I. O item compasso místico é um esotérico que permite excluir um alvo da área afetada por uma magia lançada. Se a magia não é instantânea, como Controlar Plantas, este alvo é afetado em turnos subsequentes? Isso incluiria magias que afetam o ambiente, como Controlar o Clima?<br>II. Adoramos usar as falhas críticas de Heróis de Arton, mas algumas nos confundem. Por exemplo, o resultado 74 da tabela menciona um item alquímico arremessado que cai. Mas não usamos jogadas de ataque para itens alquímicos! Como fica?<br>III. O poder Mão Amiga pode ser usado para agilizar o tempo de treinamento para uma distinção?",
+        "conselheiro": "— Cons. Deryk Feijoo",
+        "resposta": "Saudações heroicas, conselheiro! Vamos às suas respostas:<br>I. Você escolhe qual alvo é excluído da área ao lançar a magia, mas isto o beneficiará enquanto a área for afetada pela magia (independentemente da magia ter duração instantânea ou de 1 ou mais rodadas).<br>II. Como mencionado na página 300 de Heróis de Arton, nem todos os efeitos estão diretamente ligados à jogada de ataque realizada.<br>III. Desde que a distinção em questão permita que o personagem receba ajuda, sim."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "magia",
+          "racas",
+          "regras"
+        ],
+        "pergunta": "Em Deuses de Arton, a descrição dos dragonetes aponta que tais seres feéricos podem ser familiares, mas a ficha de tal criatura traz apenas opções para parceiro. Quais seriam os benefícios concedidos para um arcanista (bem paciente) que tenha um dragonete como familiar?",
+        "conselheiro": "— Cons. Pietro Vicari",
+        "resposta": "Saudações feéricas, conselheiro! Parece que as artimanhas dos dragonetes se acometeram sobre todos nós. Até desvendarmos o que exatamente aconteceu, desconsidere menções a dragonetes familiares e use-os apenas como parceiros, como apontado na ficha em Deuses de Arton."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "regras",
+          "classes"
+        ],
+        "pergunta": "Saudações, caros membros da STR!<br>Minha dúvida é mais conceitual do que mecânica. Por que apenas Thwor concede o poder Fúria Divina? Megalokk é o deus dos monstros e da ferocidade, cujos druidas são responsáveis por criarem gigantes furiosos. Não faria sentido que concedesse o mesmo poder? Desde já agradeço.",
+        "conselheiro": "— Cons. Jorge",
+        "resposta": "Saudações ferozes, conselheiro! Embora os devotos de Megalokk sejam brutais, lhes falta a clareza de propósito daqueles que almejam pelo Mundo Como Deve Ser. Dessa forma, não são capazes de direcionar sua fúria de forma tão eficiente. Além disso, note que esse poder também é concedido por Keenn, caso você jogue uma aventura no passado de Arton (como Guerra Artoniana), antes da ascensão de Arsenal."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "magia",
+          "classes",
+          "itens",
+          "regras"
+        ],
+        "pergunta": "Prezados membros do Supremo Tribunal Regreiro (STR), venho, respeitosamente, submeter à apreciação deste tribunal novas dúvidas relacionadas à classe variante alquimista, principalmente, conforme descrita em Tormenta20 e materiais complementares, visando assegurar a correta interpretação das regras em mesa.<br>I. Dissipar Magia tem como função encerrar efeitos ativos de magias. Considerando que poções produzem efeitos equivalentes a magias quando consumidas, questiona-se: os efeitos provenientes de poções podem ser dissipados normalmente por Dissipar Magia, ou são tratados de forma distinta por sua natureza alquímica?<br>II. Indo na mesma linha de pensamento, efeitos mágicos no geral, como a Forma Selvagem do druida e a Égide do paladino, seriam dissipados por Dissipar Magia ou não seriam dissipados por não serem magias propriamente ditas?<br>III. Em discussões anteriores do STR, foi mencionado que emulsões não são itens alquímicos. Contudo, conforme descrito em Heróis de Arton, as emulsões utilizam as regras de fabricação de alquímicos, embora com custos e CDs próprios. Diante disso, questiona-se: é possível utilizar o poder Catalizador Instável para fabricar uma emulsão como ação completa, mediante o pagamento dos custos em PM e tibares correspondentes?<br>Agradeço pela atenção e pela contínua dedicação deste tribunal em manter a harmonia e clareza das regras do sistema. Atenciosamente.",
+        "conselheiro": "— Cons. Jéssica Niohanne",
+        "resposta": "Saudações metálicas, conselheira! Vamos às suas respostas:<br>I. Sim, podem ser dissipados normalmente, pois poções geram o efeito de magias ao serem usadas (Tormenta20, p. 341).<br>II. Não, apenas magias.<br>III. Emulsões são itens alquímicos, elas só não são preparados alquímicos (como esclarecemos anteriormente). Como Catalizador Instável permite fabricar preparados alquímicos ou poções, não funciona com emulsões."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "racas",
+          "regras",
+          "combate"
+        ],
+        "pergunta": "Olá, digníssimo tribunal! Tenho uma dúvida. Estou querendo jogar como um duende, de Heróis de Arton. A habilidade Metamorfose Animal me permite virar um bicho e ganhar armas naturais. Elas contam para os poderes Arma Natural Aprimorada/Hábil? O texto dos poderes de armas naturais diz que o pré-requisito é \"arma natural fornecida por raça\". Como a Metamorfose é uma habilidade racial do duende, queria saber se é possível essa interação, mesmo recebendo as armas naturais de forma indireta.",
+        "conselheiro": "— Cons. Godrakke",
+        "resposta": "Saudações novamente feéricas, conselheiro! Pela leitura mais rígida da regra, a raça não concede uma arma natural, dá uma habilidade que por sua vez concede uma arma natural. Então, não. Porém, como sempre aconselhamos, converse com o seu mestre! Talvez ele ache a ideia interessante."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "combate",
+          "regras"
+        ],
+        "pergunta": "Saudações, nobres juízes!<br>Estamos usando a regra adicional de efeitos críticos, de Heróis de Arton. Porém, temos dúvidas sobre margens de crítico ampliadas. Os efeitos críticos se aplicam sempre que consigo um acerto crítico (no caso do meu personagem, com um resultado de 12 ou mais) ou apenas com um 20 natural?",
+        "conselheiro": "— Cons. Christian Gross",
+        "resposta": "Saudações decisivas, conselheiro! Os efeitos se aplicam com qualquer acerto crítico, não é necessário um 20 natural."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "classes",
+          "combate",
+          "regras"
+        ],
+        "pergunta": "Saudações, nobres juízes!<br>Uma dúvida há muito vem consumindo a minha mente. Como funciona a Aura de Invencibilidade do paladino, quando ele é um dos últimos na iniciativa e recebe dano? Se ele ativa o poder depois de sofrer dano, cancelaria o dano que já sofreu nessa mesma rodada? Caso contrário o poder me parece inútil, especialmente por ser acessível apenas no nível 18!",
+        "conselheiro": "— Cons. rlsrenegado",
+        "resposta": "Saudações sagradas, conselheiro! Depois de ativada, a Aura de Invencibilidade ignora o próximo dano sofrido na cena (não na rodada, nem antes dele ser ativado) para o paladino e seus aliados. Exceto caso o combate termine na primeira rodada, ainda há muito valor a ser obtido deste poder: lembrando que ativar a aura de paladino consome apenas 1 PM."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "regras",
+          "combate",
+          "classes"
+        ],
+        "pergunta": "Caros juízes, venho com uma dúvida sobre o que fazer numa situação específica como mestre. Faz algum tempo, mestrei uma campanha em que um dos meus jogadores fez um treinador que tinha um lobo gigante como melhor amigo e utilizava como montaria. Quando meus monstros e NPCs atacavam, porém, eu ficava em dúvida: deveria atacar o treinador ou sua montaria? Se vocês estivessem nessa mesma situação, o que me recomendariam fazer?",
+        "conselheiro": "— Cons. MoonKnight da Silva",
+        "resposta": "Saudações fraternas, conselheiro! Essa é uma questão que deve levar em conta quais inimigos estão enfrentando o treinador, além das estatísticas dos dois personagens. Se o lobo tiver Defesa alta, faz sentido que qualquer atacante tente mirar no treinador em vez dele. Um conjurador pode decidir concentrar seus ataques no treinador desde o começo, para impedir que alguém direcione a ferocidade do animal. Se por um acaso for um caçador que odeie lobos, com certeza os ataques no melhor amigo terão prioridade! É preciso definir caso a caso."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "combate",
+          "classes",
+          "regras"
+        ],
+        "pergunta": "Juízes! Poderiam esclarecer algumas dúvidas desse aventureiro?<br>I. O poder de bárbaro Rigidez Selvagem, de Heróis de Arton, permite aplicar o bônus de Fúria à Defesa. Ao alcançar o nível 20, o poder Fúria Titânica dobra os bônus de Fúria nos testes de ataque e dano. Nesse caso, o bônus concedido por Rigidez Selvagem à Defesa também é dobrado?<br>II. Um guerreiro com os poderes Ambidestria e Operações Combinadas pode realizar dois ataques ao utilizar Operações Combinadas?<br>III. O poder Escola de Duelo: Escola Ambidestra, do duelista, já permite o ataque com duas armas ou precisa do poder Estilo de Duas Armas?",
+        "conselheiro": "— Cons. Djow_Tromundo",
+        "resposta": "Saudações marciais, conselheiro! Vamos às suas respostas:<br>I. Sim. \"Bônus de fúria\" é uma forma resumida de se referir ao bônus concedido pela habilidade Fúria nos testes de ataque e rolagens de dano corpo a corpo. Fúria Titânica dobra esse bônus, que por sua vez é aplicado em Defesa, em Fortitude e em RD por conta de Rigidez Selvagem.<br>II. Não. Operações Combinadas oferece apenas um ataque. Por exemplo, em seu turno, um guerreiro com Ambidestria pode gastar sua ação padrão para realizar a ação agredir e fazer dois ataques. Se ao menos um deles acertar um alvo sob efeito de seu Xadrez de Batalha, pode usar Ordens de Engajamento para fornecer um ataque extra para um aliado em alcance curto e Operações Combinadas para ganhar um ataque extra (não uma ação padrão) a ele mesmo, no mesmo momento.<br>III. Não. O benefício é apenas aquele listado na descrição. Para fazer um ataque adicional com duas armas é necessário ter Ambidestria ou Estilo de Duas Armas."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "magia",
+          "regras"
+        ],
+        "pergunta": "Excelentíssimos doutores, na DB Especial Ghanor fomos apresentados à tradição primal. Qual seria o efeito do poder de mago Raio da Tradição em relação a essa tradição?",
+        "conselheiro": "— Cons. Infahur",
+        "resposta": "Saudações escatológicas, conselheiro! Raio da Tradição não tem efeito com a tradição primal como escrita. Porém, caso seu mestre concorde, você pode usar o efeito de deixar o alvo enfeitiçado por 1 rodada."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "regras",
+          "classes"
+        ],
+        "pergunta": "Bom dia, caros juízes, trago algumas perguntas sobre regras de culinária e alimentação.<br>I. O poder Cozinheiro da Abadia permite combinar os efeitos de dois pratos em um. Nesse caso, dois banquetes dos heróis concederiam +1 em dois atributos diferentes?<br>II. E quanto a Ás da Cozinha?<br>III. Se o personagem tiver Cozinheiro da Abadia e Ás da Cozinha, poderia combinar três pratos em um só, chegando a três banquetes dos heróis?<br>IV. Um personagem com Bom de Garfo poderia se beneficiar de dois banquetes dos heróis no mesmo dia, desde que cada banquete concedesse bônus a atributos diferentes?",
+        "conselheiro": "— Cons. Samkim Aura Monster",
+        "resposta": "Saudações gastronômicas, conselheiro! Vamos às suas respostas:<br>I. Não. O poder permite combinar os efeitos de dois pratos, mas banquete de heróis é apenas um prato.<br>II. Não, da mesma forma.<br>III. O personagem poderia combinar três pratos em um só, mas não fazer com que o mesmo prato conceda o seu benefício três vezes.<br>IV. Sim."
+      },
+      {
+        "sistema": "Tormenta20",
+        "tags": [
+          "combate",
+          "classes",
+          "itens",
+          "regras"
+        ],
+        "pergunta": "Olá, meritíssimos. Seguindo os conselhos de Bilu, um famoso clérigo de Tanna-Toh, venho aqui mais uma vez em busca de conhecimento:<br>I. Caso eu tenha um Golpe Pessoal Conjurador com as magias Toque Vampírico ou Toque Chocante, eu poderia pagar os PMs da melhoria que permite realizar um ataque como parte da execução da magia? Ou seja, usar o Golpe Pessoal, ativar a magia e depois realizar o ataque da execução da magia, resultando em dois ataques em um único movimento? Se sim, os custos desses PMs das melhorias da magia entram no limite de PMs do Golpe Pessoal?<br>II. Personagens sem as devidas proficiências em armaduras e escudos aplicam a penalidade de armadura nas perícias baseadas em Força e Destreza. Isso quer dizer que um personagem não proficiente poderia utilizar uma brigantina (Heróis de Arton) sem sofrer nenhuma penalidade adicional?<br>III. Heróis de Arton trouxe encantos para acessórios, dando a eles categorias como instrumentos musicais, vestuário e afins. Surgiram duas dúvidas: esses acessórios, ao entrarem nessas categorias, poderiam receber melhorias de itens superiores? Por exemplo, o manto do fascínio poderia receber melhorias como inscrito ou cravejado de gemas? Ou ainda: a própria flauta fantasma, que agora consta como instrumento musical, poderia, na mão de um bardo, receber melhorias de esotéricos?<br>IV. Infelizmente, no Guia dos Deuses Menores não tivemos a presença da Sagrada Bola de Fogo. Existe alguma chance dessa calorosa divindade aparecer em algum suplemento futuro?<br>V. Agora uma pergunta trivial: klirens têm seis dedos?",
+        "conselheiro": "— Cons. Renan Orange",
+        "resposta": "Saudações alienígenas, conselheiro! Vamos às suas respostas:<br>I. Sim, você pode usar um Golpe Especial Conjurador para usar Toque Chocante ou magias similares e fazer um ataque adicional com a magia. Isso resulta em dois ataques, sendo que um deles aplica o efeito da magia. O custo em PM da magia faz parte do limite de PM de Golpe Pessoal.<br>II. Sim, assim como qualquer personagem usando uma armadura com penalidade de armadura zero. Lembre-se, porém, de que existem outras questões envolvendo uso de armaduras pesadas como a brigantina: não se soma Destreza na Defesa, o deslocamento é reduzido em 3m, é necessário um teste para poder lançar magias arcanas.<br>III. Sim. Para todos os propósitos, os acessórios são itens das categorias adequadas.<br>IV. Não há notícias recentes sobre a Sagrada Bola de Fogo. Mas uma coisa é certa: seus fiéis continuam tão explosivos quanto nunca!<br>V. Existem klirens com seis dedos, mas nem todo kliren tem seis dedos."
+      }
+    ]
+  },
+  {
     "id": "db227",
     "label": "DB 227 (Mai/26)",
     "artigos": [
